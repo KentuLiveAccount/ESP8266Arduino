431873715bf6d0bdfe06c55f963eb6e8ba67812a
Kentaro Urata <kentu@microsoft.com>
2021-02-16 01:03:11 -0800

    internal temp & servo support

--- jsonloggerNS/jsonloggerNS/jsonloggerNS.ino
+++ jsonloggerNS/jsonloggerNS/jsonloggerNS.ino
@@ -1,15 +1,20 @@
+
 /*
   HelloServerBearSSL - Simple HTTPS server example
   This example demonstrates a basic ESP8266WebServerSecure HTTPS server
   that can serve "/" and "/inline" and generate detailed 404 (not found)
   HTTP respoinses.  Be sure to update the SSID and PASSWORD before running
   to allow connection to your WiFi network.
-  Adapted by Earle F. Philhower, III, from the HelloServer.ino example.
+  Adapted by Earle F. Philhowe
+  r, III, from the HelloServer.ino example.
   This example is released into the public domain.
 */
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <Servo.h>
+#include <PID_v1.h>
+#include <math.h>
 
 
 #ifndef STASSID
@@ -17,9 +22,39 @@
 #define STAPSK  "----"
 #endif
 
+#define ServoPin 14   //D5 is GPIO14
+#define DemultiplexPin 12 // D6 is GPIO12
+
 #define TC_PIN A0          // set to ADC pin used
 #define AREF 3.1159           // set to AREF, typically board voltage like 3.3 or 5.0
 #define ADC_RESOLUTION 10  // set to ADC bit resolution, 10 is default
+
+double V_0 = 3.291;
+
+// WeMos D1 Mini internally contains voltage divider for A0 input in order to expand the voltage
+// range of ESP8266 which is limited to 1.0V
+// R_2 of the divider (between A0 and GND) is fixed 100k ohm
+double R_2 = 100000.0; //VCC - R_1 - A0 - R_2 - GND (R_1 = 230 + rThermistor)
+
+// D1 Mini also has 220k ohm on the VCC side of the divider
+double R_1d = 220000.0;
+
+// I'm adding additional 10k register on VCC side of the divider, along with the sensor
+// to make sure input voltage to the A0 pin never exceed 1V.
+double R_1Adj = 10000.0;
+
+
+double factor = 10000000.0;
+double CM0 = 9621.996628; //0.9621996628 e-3
+double CM1 = 1800.586745; //1.800586745 e-4
+double CM2 = 1.97132282; //1.971322820 e-7 
+
+double sensorMax = 1.0;
+Servo myservo;
+
+IPAddress local_IP(192, 168, 1, 4);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 0, 0);
 
 float get_voltage(int raw_adc) {
   float raw = raw_adc;
@@ -40,17 +75,29 @@ ESP8266WebServer server(80);
 
 const int cTempMax = 100;
 int temps[cTempMax];
+int intTemps[cTempMax];
 int target[cTempMax];
 int cTemp = 0;
 int millisTimeLast = 0;
-int targetTemp = 100;
+double targetTemp = 100;
+double currentTemp = 0;
+double currentInternalTemp = 0;
+double angle = 0;
+
+PID myPID(&currentTemp, &angle, &targetTemp,2,5,1, DIRECT);
 
 const int led = 2;
 
-void AddTemp(int temp)
+void AddTemp(int temp, int tempInt)
 {
   temps[(cTemp % cTempMax)] = temp;
+  intTemps[cTemp % cTempMax] = tempInt;
   target[(cTemp++ % cTempMax)] = targetTemp;
+}
+
+void UpdateCurrentTarget(int newTarget)
+{
+  target[(cTemp + cTempMax - 1) % cTempMax] = newTarget;
 }
 
 String ReadTemps()
@@ -63,6 +110,8 @@ String ReadTemps()
   {
     str += "{ \"currenttemp\": ";
     str += String(temps[i % cTempMax]);
+    str += ", \"internatemp\": ";
+    str += String(intTemps[i % cTempMax]);
     str += ", \"targettemp\": ";
     str += String(target[i++ % cTempMax]);
     str += "}";
@@ -100,6 +149,8 @@ void handleSetTemp() {
   server.sendHeader("Access-Control-Allow-Origin","*");
   server.send(200, "text/plain", "temp set");
   targetTemp = server.arg("plain").toInt();
+  UpdateCurrentTarget(targetTemp);
+
   delay(200);
   digitalWrite(led, LOW);
 
@@ -124,9 +175,21 @@ void handleNotFound(){
 
 void setup(void){
   pinMode(led, OUTPUT);
+  pinMode(ServoPin, OUTPUT);
+  pinMode(DemultiplexPin, OUTPUT);
   pinMode(A0, INPUT) ;
   digitalWrite(led, HIGH);
   Serial.begin(115200);
+  
+  myservo.attach(ServoPin);
+  myservo.write(0);
+
+  myPID.SetMode(AUTOMATIC);
+
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+    Serial.println("STA Failed to configure");
+  }
+
   WiFi.begin(ssid, password);
   Serial.println("");
 
@@ -156,22 +219,99 @@ void setup(void){
   digitalWrite(led, LOW);
 }
 
-void loop(void){
-  server.handleClient();
-  int millisNow = millis();
-  if (millisTimeLast == 0 || millisNow - millisTimeLast > (1000 * 30))
-  {
-    millisTimeLast = millisNow;
-    int reading = analogRead(A0);
+float readThermoCouple()
+{
+    digitalWrite(DemultiplexPin, LOW);
+
+    int reading = 0;
+    for (int i = 0; i < 10; i++)
+    {
+      delay(10);
+      reading = reading + analogRead(A0);
+    }
+
+    reading = reading / 10;
+
     Serial.println("\nreading: " + String(reading));
     float voltage = get_voltage(reading);
     Serial.println("\nvoltage: " + String(voltage));
-    int tempC  = get_temperature(voltage);
-    Serial.println("\ntempC: " + String(tempC));
+    return get_temperature(voltage);
+}
 
-    int tempF  = tempC * 9 / 5 + 32;
-    Serial.println("\ntempF: " + String(tempF));
-    AddTemp(tempF);
+float readThermistor()
+{
+  digitalWrite(DemultiplexPin, HIGH);
+
+  // read the pin value 10 times over 100 ms span and take average
+  double sensorValue = 0;
+  for (int i = 0; i < 10; i++)
+  {
+    delay(10);
+    // read the input on analog pin 0:
+    sensorValue += analogRead(A0);
+  }
+
+  sensorValue = sensorValue / 10;
+
+  Serial.println("\nTermistorSensor: " + String(sensorValue ));
+
+  // Convert the analog reading (which goes from 0 - 1023) to voltage reference (3.3V or 5V or other):
+  double voltage = sensorValue * sensorMax /1023.0;
+
+  // i'm adjusting the misterious temperature error (for now treating it as constant error)
+  voltage = voltage - 0.028;
+
+  double R_1 = R_2 * (V_0 - voltage)/voltage; // voltage to resistance
+
+  // remove the fixed 230k ohm on the vcc side of the voltage dividor
+  double rThermistor = R_1 - R_1d - R_1Adj;
+
+  Serial.println("\nTermistorOhm: " + String(rThermistor));
+  if (rThermistor < 0)
+    return 0;
+
+  //rThermistor = 133000;
+  double lnR = log(rThermistor);
+
+  // T(K) = 1/(C0 + C1Ln(R) + C2(Ln(R))^3)
+  double temp  = factor /(CM0 + lnR * CM1 + CM2 * lnR * lnR * lnR);
+  temp  = temp - 273.15; // K -> C
+
+  if (temp > 300)
+    temp = 0;
+
+  return temp;
+}
+
+void loop(void){
+  server.handleClient();
+  int millisNow = millis();
+  if (millisTimeLast == 0 || millisNow - millisTimeLast > (1000 * 10))
+  {
+    millisTimeLast = millisNow;
+    int tempC  = readThermoCouple();
+    Serial.println("\nX-tempC: " + String(tempC));
+
+    currentTemp  = tempC * 9 / 5 + 32;
+    Serial.println("\nX-tempF: " + String(currentTemp));
+
+    tempC = readThermistor();
+    Serial.println("\nI-tempC: " + String(tempC));
+
+    currentInternalTemp  = tempC * 9 / 5 + 32;
+    Serial.println("\nI-tempF: " + String(currentInternalTemp));
+    AddTemp(currentTemp, currentInternalTemp);
     Serial.println("\ntemps: " + ReadTemps());
+    
+
+    myPID.Compute();
+
+    int angleI = angle > 105 ? 105 : angle;
+    angleI = angleI < 0 ? 0 : angleI;
+
+    Serial.println("\nservo angle: " + String(angleI));
+
+    myservo.write(angleI);
+
   }
 }
