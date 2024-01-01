ab7d23b4943888847ecd35997b3dc1abd49fd8ce
Kentaro Urata <kentu@microsoft.com>
2024-01-01 12:12:59 -0800

    add comment, delete unnecessary file

--- 8266i2saudio/8266i2sSamplePlay/8266i2sSamplePlay.ino
+++ 8266i2saudio/8266i2sSamplePlay/8266i2sSamplePlay.ino
@@ -5,16 +5,24 @@
 #include <pgmspace.h>
 
 #include "sampledata.h"
+// sampled data are signed 16bit values
 //const uint16_t BD16[144000] PROGMEM = {
 //};
 
 const uint32_t countMax = sizeof(BD16)/sizeof(uint16_t);
 
 void setup() {
+  // turn off wifi
   WiFi.forceSleepBegin();
   delay(1);
+
+  // set CPU to opearate at 160MHz
   system_update_cpu_freq(160);
+
+  // 16bit per sample per channel
+  i2s_set_bits(16);
   i2s_begin();
+  // sampling requency == 44.1kHz
   i2s_set_rate(44100);
 }
 
@@ -22,12 +30,16 @@ uint32_t counter = 0;
 
 void loop()
 {
+ // use pgm_read_word_near to read off of the array in PROGMEM
  int16_t DAC= pgm_read_word_near(BD16 + counter++);
 
  if (counter >= countMax)
   counter = 0;
 
- uint32_t lrv = DAC;
- lrv = (lrv & 0xffff) << 16 | (lrv) & 0xffff;
+ uint32_t left = DAC;
+ uint32_t right = DAC;
+
+ //left half of the value is the sample value for left, right half is for right
+ uint32_t lrv = ((left & 0xffff)) << 16 | (right) & 0xffff;
  i2s_write_sample(lrv);    
 }
