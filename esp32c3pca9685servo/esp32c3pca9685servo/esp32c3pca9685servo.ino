b4091b7ada46e1e10962bb4a8f954e812d4c262d
Kentaro Urata <kentu@microsoft.com>
2024-09-06 20:21:39 -0700

    add comment, frequency -> 50hz

--- esp32c3pca9685servo/esp32c3pca9685servo/esp32c3pca9685servo.ino
+++ esp32c3pca9685servo/esp32c3pca9685servo/esp32c3pca9685servo.ino
@@ -1,6 +1,22 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
+// ESP32C3 with PCA9685 servo driver breakout
+/*
+  Wiring
+
+  ESP32C3   PCA9685
+  GND    -> GND
+  3V3    -> VCC
+  GPIO5  -> SDA
+  GPIO4  -> SCL
+  3V3    -> OE (via switch: ON -> NO POWER to Device, leave open to power device)
+
+  External power (5V) to V+ and GND to power devices
+
+  Note: observing occasional erratic servo movement. supplying separate power to sservo does not help
+*/
+
 Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40, Wire);
 
 const int sdaPin = 5;
@@ -11,11 +27,12 @@ void setup() {
   Serial.println("16 channel PWM test!");
 
   pwm1.begin();
-  pwm1.setPWMFreq(60);  // This is the maximum PWM frequency
+  pwm1.setPWMFreq(50);  // SG90 operates at 50Hz
 }
 
 void loop()
 {
+  // servo sweep from 0 to 180 maps to 120 - 660
   for (int i = 120; i < 661; i += 5)
   {
     Serial.printf("pwm 0: 0, %d\n", i);
